d01b27e93a4c8cebeda4ecd22df7adafd4be69bb
sammrguez <sammrguez@gmail.com>
2024-04-16 21:20:39 -0600

    return token

--- controllers/users.js
+++ controllers/users.js
@@ -2,55 +2,89 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = '3199a1ee12430d5ea077aa8585caf1e8';
+
 module.exports.createUserAndLogin = (req, res) => {
   console.log(req.body);
   const { name, email, googleId } = req.body;
-  User.findOne({ email }).then((user) => {
-    if (!user) {
-      bcrypt
-        .hash(googleId, 10)
-        .then((hash) => {
+  return User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        console.log('no hay user, se creara uno');
+
+        return bcrypt.hash(googleId, 10).then((hash) => {
           return User.create({
             name,
             email,
             hashedGoogleId: hash,
-          });
-        })
-        .then((user) => {
-          res.send(user);
-        })
-        .catch((err) => {
-          res.send(err);
+          })
+            .then((user) => {
+              res.send(user);
+            })
+            .catch((err) => {
+              res.send(err);
+            });
         });
-    }
-    {
-      console.log('este user ya existe');
-    }
-  });
+      } else {
+        console.log('si hay user, se iniciara sesion');
+
+        return bcrypt
+          .compare(googleId, user.hashedGoogleId)
+          .then((matched) => {
+            if (!matched) {
+              return Promise.reject(new Error('error al iniciar sesion'));
+            }
+            return user;
+          })
+          .then((user) => {
+            const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+              expiresIn: '7d',
+            });
+            res.send(token);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //   User.findOne({ email }).then((user) => {
+  //     if (!user) {
+  //       console.log('cayo en bloque crear user');
+  //       bcrypt
+  //         .hash(googleId, 10)
+  //         .then((hash) => {
+  //           return User.create({
+  //             name,
+  //             email,
+  //             hashedGoogleId: hash,
+  //           });
+  //         })
+  //         .then((user) => {
+  //           res.send(user);
+  //         })
+  //         .catch((err) => {
+  //           res.send(err);
+  //         });
+  //     } else {
+  //       console.log('cayo en bloque iniciar sesion');
+  //       const token = login(email, googleId);
+  //       res.send(token);
+  //     }
+  //   });
 };
 
-function login(req, res) {
-  const { name, email, googleId } = req.body;
-  User.findOne({ email }).then((user) => {
-    if (!user) {
-      bcrypt
-        .hash(googleId, 10)
-        .then((hash) => {
-          return User.create({
-            name,
-            email,
-            hashedGoogleId: hash,
-          });
-        })
-        .then((user) => {
-          res.send(user);
-        })
-        .catch((err) => {
-          res.send(err);
-        });
-    }
-    {
-      return bcrypt.compare(googleId, user.hashedGoogleId);
-    }
-  });
+function login(email, googleId) {
+  return User.findUserByCredentials(email, googleId)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, JWT_SECRET, {
+        expiresIn: '7d',
+      });
+      return token;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
